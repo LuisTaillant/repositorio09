7f0c43c53b077173489ba2a0a6076db9169a8b86
Luis Taillant <luis.marcelo.taillant@gmail.com>
2022-10-14 21:42:26 -0300

    Proyecto 02-Herencia HTML

---
+++ DesafioClase21/home/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+# from .views import hola, fecha
+from home import views
+# from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    # path('hola/', views.hola, name='hola'),
+    # path('fecha/', views.fecha, name='fecha'),
+    # path('fecha-nacimiento/<int:edad>', views.calcular_fecha_nacimiento),
+    # path('mi-template/', views.mi_template, name='mi_template'),
+    # path('mi-template/<str:nombre>', views.tu_template),
+    # path('prueba-template/', views.prueba_template),
+    path('ver-personas/', views.ver_personas, name='ver_personas'),
+    path('crear-persona/', views.crear_persona, name='crear_persona'),
+    # path('hola/', hola),
+    # path('fecha/', fecha),
+]
--- DesafioClase21/home/views.py
+++ DesafioClase21/home/views.py
@@ -1,3 +1,55 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from datetime import datetime
+from django.template import Context, Template, loader
+from django.shortcuts import render, redirect
+import random
+from home.forms import PersonaFormulario, BusquedaPersonaFormulario
+from home.models import Persona
 
 # Create your views here.
+def crear_persona(request):
+    
+    if request.method == 'POST':
+        
+        formulario = PersonaFormulario(request.POST)
+        
+        if formulario.is_valid():
+            data = formulario.cleaned_data
+        
+            nombre = data['nombre']
+            apellido = data['apellido']
+            edad = data['edad']
+            # v1
+            fecha_creacion = data['fecha_creacion']
+            if not fecha_creacion:
+                fecha_creacion = datetime.now()
+            
+            # v2
+            # fecha_creacion = data['fecha_creacion'] or datetime.now()
+            
+            persona = Persona(nombre=nombre, apellido=apellido, edad=edad, fecha_creacion=fecha_creacion)
+            persona.save()
+            
+            return redirect('ver_personas')
+    
+    formulario = PersonaFormulario()
+    
+    return render(request, 'home/crear_persona.html', {'formulario': formulario})
+
+
+def ver_personas(request):
+    
+    nombre = request.GET.get('nombre', None)
+    
+    if nombre:
+        personas = Persona.objects.filter(nombre__icontains=nombre)
+    else:
+        personas = Persona.objects.all()
+    
+    formulario = BusquedaPersonaFormulario()
+    
+    return render(request, 'home/ver_personas.html', {'personas': personas, 'formulario': formulario})
+
+def index(request):
+    
+    return render(request, 'home/index.html')
